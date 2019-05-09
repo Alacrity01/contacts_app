eae6010bb4d3d95021d6d57eb9e95225c7651c1a
TheAlacrity <brinker.jeff@gmail.com>
2019-05-09 17:02:57 -0500

    various changes that escape my memory

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,8 +4,7 @@ class Contact < ApplicationRecord
   validates :last_name, presence: true
 
   validates :email, uniqueness: true
-
-  validates :email, format: {with: /.@./, message: "Must be a valid email"}
+  validates :email, format: {with: /\A\w+[@]{3,}[.]w{2,}\z/, message: "must be a valid email"}
 
   def friendly_updated_at
     updated_at.strftime("%b%e,%l:%M%p")
